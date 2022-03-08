6cf926fe494a57904b0bdff7fe663097d69be310
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-08 15:24:07 +0100

    un_ange_passe

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 15:48:12 by faventur          #+#    #+#             */
-/*   Updated: 2022/03/08 11:55:46 by faventur         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:23:45 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,17 @@ size_t	ft_linelen(const char *str)
 char	*trim_and_stock(char *str)
 {
 	char	*s;
+	char	*rest;
 	int		i;
-	int		len;
 
 	i = 0;
-	len = ft_linelen(str);
-	printf("%d", len);
-	s = malloc(sizeof(char) * (len + 1));
+	s = malloc(sizeof(char) * (ft_linelen(str) + 1));
 	while (str[i] != '\n')
 	{
 		s[i] = str[i];
-		printf("%c\n", s[i]);
 		i++;
 	}
-	free(str);
+	printf("read1: %s\n", str);
 	s[i++] = '\n';
 	printf("%s\n", s);
 	s[i] = '\0';
@@ -63,14 +60,17 @@ char	*get_next_line(int fd)
 	char		*ret;
 
 	reading_buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	read_bytes = 1;
 	if (fd)
 	{
-		while (read_bytes = read(fd, &buffer, BUFFER_SIZE))
+		while (read_bytes)
 		{
+			read_bytes = read(fd, &buffer, BUFFER_SIZE);
 			buffer[read_bytes] = '\0';
-			printf("buffer: %s\n", buffer);
-			if (ft_strchr(reading_buf, '\n') != NULL)
+			if (ft_strchr(buffer, '\n') != NULL)
 			{
+				reading_buf = ft_strjoin(reading_buf, buffer);
+				printf("read: %s\n", reading_buf);
 				ret = trim_and_stock(reading_buf);
 				printf("ret: %s\n", ret);
 				return (ret);
@@ -78,14 +78,9 @@ char	*get_next_line(int fd)
 			else
 			{
 				reading_buf = ft_strjoin(reading_buf, buffer);
-				printf("read: %s\n", reading_buf);
+//				printf("read: %s\n", reading_buf);
 			}
 		}
-		
-			/*
-			if (bytes_to_read == BUFFER_SIZE || !(buffer[0]))
-				bytes_read = read(fd, &buffer, MAX_SIZE);
-			*/
 	}
 	return (NULL);
 }
@@ -95,7 +90,7 @@ int	main()
 	int		bytes_read;
 	int		fd;
 	char	*buf;
-	char 	*krum;
+	char	*krum;
 	char	*dash;
 
 	fd = open("text.txt", O_RDONLY);
@@ -103,14 +98,14 @@ int	main()
 		return (1);
 	buf = get_next_line(fd);
 	krum = get_next_line(fd);
-	dash = get_next_line(fd);
+//	dash = get_next_line(fd);
 
 	close(fd);
 	printf("return 1: %s", buf);
 	printf("return 2: %s", krum);
-	printf("return 3: %s", dash);
+//	printf("return 3: %s", dash);
 	free(buf);
 	free(krum);
-	free(dash);
+//	free(dash);
 	return (0);
 }
